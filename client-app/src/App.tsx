227a7f9ed7a555c56b99afd8fe7637936385fc2c
Thato1016 <tb.khosa@gmail.com>
2022-08-03 15:53:28 +0200

    Java

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState ([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
