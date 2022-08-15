f16e497d43963995e99760a941c94efdee0d10cc
Thato1016 <tb.khosa@gmail.com>
2022-08-15 14:44:19 +0200

    Task 38

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,6 +48,7 @@ namespace API
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
